928b3f16fbef51a2016fdf949b68ca7d0ce28818
Joyjit2001 <34081495+Joyjit2001@users.noreply.github.com>
2021-12-19 21:35:17 -0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -14,7 +14,7 @@ import SortNumber
 
 @server.route('/')
 def greet_post():
-    return "You Did it. It was all worth it!"
+    return "You Did it. It was all very worth it!"
 
 @server.route('/sort-numbers', methods = ['GET'])
 def sort_numbers():
