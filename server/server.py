c792a6e95f70c71f7c79246ff4c3e01623f1e449
Joyjit2001 <34081495+Joyjit2001@users.noreply.github.com>
2021-12-26 10:05:11 -0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -32,7 +32,7 @@ def sort_numbers():
 
 @server.route('/sort-numbers', methods = ['POST'])
 def greet_post():
-    return "You Just POSTED. Congrats!"
+    return "You Just POTED. Congrats!"
 
 @server.route('/sort-numbers', methods = ['PUT'])
 def greet_put():
